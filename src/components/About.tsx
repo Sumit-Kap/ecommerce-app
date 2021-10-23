117a4821f0ec647ae8335b3235e32b85e4e67039
Sumit Kapoor <sumwin@Sumits-MacBook-Air.local>
2021-10-23 18:43:18 +0530

    push changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,6 +3,6 @@ import React from "react";
 export default class About extends React.Component {
   render() {
     console.log("printing props", this.props);
-    return <h1>About us</h1>;
+    return <h1>About us-Component</h1>;
   }
 }
